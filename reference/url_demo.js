bc6e6d98269278772cef4e41256896affb0926b5
Breyanna Vass <breyannavass@gmail.com>
2019-10-31 14:16:37 -0400

    getting root domain, host name and pathname

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -4,3 +4,13 @@ const myURL = new URL("http://mywebsite.com/hello.html?id=100&status=active");
 
 // Serialized URL
 console.log(myURL.href);
+console.log(myURL.toString());
+
+// Host => root domain
+console.log(myURL.host);
+
+// Host name => does not get PORT
+console.log(myURL.hostname);
+
+// Pathname
+console.log(myURL.pathname);
