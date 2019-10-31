36ee82b82dd2e0f6531a522224ad5eb14293ae92
Breyanna Vass <breyannavass@gmail.com>
2019-10-31 13:05:57 -0400

    making person file a class and calling the methods in the index file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const person = require("./person");
+const Person = require("./person");
 
-console.log(person.name);
+const person1 = new Person("Nancy Drew", 18);
+
+person1.greeting();
--- person.js
+++ person.js
@@ -1,6 +1,17 @@
-const person = {
-  name: "John Doe",
-  age: 40
-};
+// const person = {
+//   name: "John Doe",
+//   age: 40
+// };
 
-module.exports = person;
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greeting() {
+    console.log(`My name is ${this.name} and I am ${this.age}.`);
+  }
+}
+
+module.exports = Person;
