8b885edcfca5350230e13d1f18097d1dc663914f
Breyanna Vass <breyannavass@gmail.com>
2019-10-31 13:43:18 -0400

    reading the text file in console

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -9,21 +9,27 @@ const path = require("path");
 // });
 
 // create and write a file
-fs.writeFile(
-  path.join(__dirname, "/test", "hello.txt"),
-  "Hello World!",
-  err => {
-    if (err) throw err;
-    console.log("File written to...");
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "Hello World!",
+//   err => {
+//     if (err) throw err;
+//     console.log("File written to...");
 
-    // File append
-    fs.appendFile(
-      path.join(__dirname, "/test", "hello.txt"),
-      " I love Node.js",
-      err => {
-        if (err) throw err;
-        console.log("File written to...");
-      }
-    );
-  }
-);
+//     // File append
+//     fs.appendFile(
+//       path.join(__dirname, "/test", "hello.txt"),
+//       " I love Node.js",
+//       err => {
+//         if (err) throw err;
+//         console.log("File written to...");
+//       }
+//     );
+//   }
+// );
+
+// Read a file
+fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
