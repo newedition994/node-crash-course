07025b00d680b4254a8bbb22b3944be403a96bb9
Breyanna Vass <breyannavass@gmail.com>
2019-10-31 13:23:22 -0400

    understanding file extension, a path object and concating paths

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -5,3 +5,12 @@ console.log(path.basename(__filename));
 
 // Directory name
 console.log(path.dirname(__filename));
+
+// File extension
+console.log(path.extname(__filename));
+
+// Create path object
+console.log(path.parse(__filename));
+
+// Concatenate paths
+console.log(path.join(__dirname, "test", "hello.html"));
